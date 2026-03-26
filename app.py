bc176c9b1a5649d4e84bb143cc42694921493d05
soundarya21112004 <soundarya21112004@gmail.com>
2026-03-26 10:20:32 +0530

    modify index.html

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-tasks = []
+# DB config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Table model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+
+# Create DB
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
+    tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
 @app.route('/add', methods=['POST'])
 def add():
-    task = request.form.get('task')
-    tasks.append(task)
+    task_content = request.form.get('task')
+    new_task = Task(content=task_content)
+    db.session.add(new_task)
+    db.session.commit()
     return redirect('/')
 
-@app.route('/delete/<int:index>')
-def delete(index):
-    tasks.pop(index)
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
     return redirect('/')
 
 if __name__ == '__main__':
